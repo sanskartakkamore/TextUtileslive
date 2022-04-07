306cd521f9d9aaad084127f3238e53715e40d13b
sanskartakkamore <sanskartakkamore@gmail.com>
2022-04-07 14:21:57 +0530

    Updates

---
+++ precache-manifest.30e0ed0f50ab95fb35b7bd1c443f9b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9d5734cb2f268dcf670",
+    "url": "/TextUtileslive/static/css/main.5caacb1c.chunk.css"
+  },
+  {
+    "revision": "c9d5734cb2f268dcf670",
+    "url": "/TextUtileslive/static/js/main.99905786.chunk.js"
+  },
+  {
+    "revision": "e9c9ae7b1b29f722cafd",
+    "url": "/TextUtileslive/static/js/runtime~main.9e1dcd47.js"
+  },
+  {
+    "revision": "127b0f89d721f7a0aa54",
+    "url": "/TextUtileslive/static/js/2.8bcefe58.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/TextUtileslive/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "88aaa776e1f76f15eaa50122c6ed159d",
+    "url": "/TextUtileslive/index.html"
+  }
+];
